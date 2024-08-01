1ad3726cb017520992f371290a01a2dd5d0d6fbd
Matheus Miranda <miranda.mhs0@gmail.com>
2024-08-01 18:04:35 -0300

    GET ping route

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 Route::get('/debug', function(Request $request) {
-    $json = Storage::disk('local')->get('goat-format-cards.json');
-    $records = array_values(json_decode($json, true));
+    return response()->json([
+        // code
+    ]);
+});
 
-    foreach ($records as $cards) {
-        return response()->json($cards[2]);
-
-        foreach ($cards as $card) {
-            return response()->json($card);
-        }
-    }
-    // return response()->json($cards);
+Route::get('/ping', function(Request $request) {
+    return response()->json([
+        'message' => 'pong',
+    ]);
 });
